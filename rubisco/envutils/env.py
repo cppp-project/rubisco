cf5fad4c1b6e68d034f4cb34bb2797830cb5ae0c
ChenPi11 <wushengwuxi-msctinoulk@outlook.com>
2024-11-23 15:32:02 +0800

    Use refcount for environment locking.

--- rubisco/envutils/env.py
+++ rubisco/envutils/env.py
@@ -23,6 +23,7 @@ import enum
 import os
 import sqlite3
 import sys
+import threading
 import time
 import venv
 from pathlib import Path
@@ -36,6 +37,7 @@ from rubisco.config import (
     VENV_LOCK_FILENAME,
     WORKSPACE_EXTENSIONS_VENV_DIR,
 )
+from rubisco.envutils.env_db import RUEnvDB
 from rubisco.envutils.utils import add_venv_to_syspath, is_venv
 from rubisco.lib.exceptions import RUError, RUOSError
 from rubisco.lib.l10n import _
@@ -47,7 +49,6 @@ from rubisco.shared.ktrigger import IKernelTrigger, call_ktrigger
 __all__ = [
     "EnvType",
     "RUEnvironment",
-    "setup_new_venv",
     "GLOBAL_ENV",
     "USER_ENV",
     "WORKSPACE_ENV",
@@ -62,6 +63,32 @@ class EnvType(enum.Enum):
     GLOBAL = "global"
 
 
+_CREATE_DB_SQL = """
+CREATE TABLE extensions (
+    name VARCHAR(64) PRIMARY KEY,
+    version VARCHAR(64) NOT NULL,
+    description TEXT,
+    homepage TEXT,
+    maintainers TEXT,
+    license TEXT,
+    tags TEXT,
+    requirements TEXT
+)
+"""
+
+
+def _check_vaild_db(db_file: Path) -> bool:
+    try:
+        db_connection = sqlite3.connect(db_file)
+        db_cursor = db_connection.cursor()
+        db_cursor.execute("SELECT * FROM extensions").close()
+        db_connection.close()
+        # Else block is low readability.
+        return True  # noqa: TRY300
+    except sqlite3.Error:
+        return False
+
+
 class RUEnvironment:
     """Extension environment manager."""
 
@@ -69,6 +96,7 @@ class RUEnvironment:
     _type: EnvType
     _lockfile: Path
     db_file: Path
+    db_handle: RUEnvDB
 
     def __init__(self, path: Path, env_type: EnvType) -> None:
         """Initialize the environment manager."""
@@ -76,10 +104,62 @@ class RUEnvironment:
         self._type = env_type
         self._lockfile = self.path / VENV_LOCK_FILENAME
         self.db_file = self.path / DB_FILENAME
+        self.db_handle = RUEnvDB(self.db_file)
 
-    def _create(self) -> None:
-        if not is_venv(self.path):
-            setup_new_venv(self)
+    def create(self) -> None:
+        """Create the environment if not exists. Check it if exists.
+
+        Raises:
+            RUError: If failed to create the venv or the database.
+            RUOSError: If OS error occurred.
+
+        """
+        if self.db_file.exists() and not self.db_is_valid():
+            call_ktrigger(
+                IKernelTrigger.on_warning,
+                message=format_str(
+                    _("The database '${{path}}' is broken."),
+                    fmt={"path": str(self.db_file)},
+                ),
+            )
+        if self.path.exists() and not self.valid():
+            logger.error("The environment '%s' is broken.", self.path)
+            raise RUError(
+                _("Failed to check the environment."),
+                hint=format_str(
+                    _(
+                        "The '${{path}}' is not a valid venv but exists."
+                        "Please remove it and try again.",
+                    ),
+                    fmt={"path": str(self.path)},
+                ),
+            )
+        if not self.path.exists():
+            logger.info("Setting up a new venv: '%s'", self.path)
+            call_ktrigger(IKernelTrigger.on_create_venv, path=self.path)
+            try:
+                venv.create(self.path, with_pip=True, upgrade_deps=True)
+                (self.path / EXTENSIONS_DIR).mkdir(exist_ok=True, parents=True)
+                logger.debug("Creating database %s", self.db_file)
+                with sqlite3.connect(self.db_file) as db_connection:
+                    db_connection.execute(_CREATE_DB_SQL)
+                    db_connection.commit()
+                add_venv_to_syspath(self.path)
+            except OSError as exc:
+                raise RUOSError(
+                    exc,
+                    hint=_(
+                        "Failed to create venv. Consider to check the"
+                        "permissions of the path.",
+                    ),
+                ) from exc
+            except sqlite3.Error as exc:
+                raise RUError(
+                    exc,
+                    hint=_(
+                        "Failed to create the database for the environment.",
+                    ),
+                ) from exc
 
     @property
     def path(self) -> Path:
@@ -107,112 +187,232 @@ class RUEnvironment:
         """Check if the environment is locked."""
         return self._lockfile.exists()
 
-    def lock(self) -> None:
-        """Lock the environment."""
-        self._create()
+    def is_locked_by_self(self) -> bool:
+        """Check if the environment is locked by the current thread."""
+        if not self.is_locked():
+            return False
 
-        if self.is_locked():
-            pid = -1
-            try:
-                with Path.open(self._lockfile, encoding=DEFAULT_CHARSET) as f:
-                    pid = int(f.read().strip())
-            except ValueError:
-                pass
+        lock_pid, lock_thread_id, _rc = self._parse_lockfile()
+        is_current_thread = lock_thread_id == threading.get_ident()
+        is_current_thread &= lock_pid == os.getpid()
 
+        return is_current_thread
+
+    def _wait_lock(self) -> bool:
+        logger.info(
+            "Waiting for the environment to be unlocked ('%s')",
+            self._lockfile,
+        )
+        task_name = format_str(
+            _(
+                "Waiting for the environment to be unlocked "
+                "('${{lock_file}}'): ${{seconds}} seconds.",
+            ),
+            fmt={
+                "lock_file": str(self._lockfile),
+            },
+        )
+        call_ktrigger(
+            IKernelTrigger.on_new_task,
+            task_name=task_name,
+            task_type=IKernelTrigger.TASK_WAIT,
+            total=0,
+        )
+        wait_time = 0
+        locked = False
+        while self.is_locked() and not self.is_locked_by_self():
+            locked = True
+            call_ktrigger(
+                IKernelTrigger.on_progress,
+                task_name=task_name,
+                current=wait_time,
+                delta=False,
+                more_data={"env": self},
+            )
+            time.sleep(1)
+            wait_time += 1
+
+        call_ktrigger(
+            IKernelTrigger.on_finish_task,
+            task_name=task_name,
+        )
+
+        return locked
+
+    def _check_alerady_locked(
+        self,
+        lock_pid: int,
+        lock_thread_id: int,
+    ) -> bool:
+        is_current_thread = lock_thread_id == threading.get_ident()
+        is_current_thread &= lock_pid == os.getpid()
+
+        if not is_current_thread:  # Skip waiting if the current thread.
             logger.warning(
-                "The environment '%s' is already locked by PID %d. "
-                "If you want to force unlock it, please remove the "
-                "lock file '%s' manually.",
+                "The environment '%s' is already locked by PID %d, "
+                "thread %d, If you want to force unlock it, please remove "
+                "the lock file '%s' manually.",
                 self.path,
-                pid,
+                lock_thread_id,
+                lock_pid,
+                self._lockfile,
+            )
+            str_pid = str(_("Unknown") if lock_pid == -1 else lock_pid)
+            call_ktrigger(
+                IKernelTrigger.on_warning,
+                message=format_str(
+                    _(
+                        "The environment '[underline]${{path}}"
+                        "[/underline]' is already locked by "
+                        "PID ${{pid}}. If you want to force unlock it, "
+                        "please remove the lock file '[underline]"
+                        "${{lockfile}}[/underline]' manually.",
+                    ),
+                    fmt={
+                        "path": str(self.path),
+                        "pid": str_pid,
+                        "lockfile": str(self._lockfile),
+                    },
+                ),
+            )
+            if lock_pid != -1 and not is_valid_pid(lock_pid):
+                call_ktrigger(  # If the process has already exited, warn.
+                    IKernelTrigger.on_warning,
+                    message=format_str(
+                        _(
+                            "The PID '${{pid}}' is not valid. Maybe the "
+                            "process has already exited."
+                            " Remove the lock file if you believe the"
+                            " process has already exited.",
+                        ),
+                        fmt={
+                            "pid": str(lock_pid),
+                        },
+                    ),
+                )
+
+            return self._wait_lock()
+        return False
+
+    def _parse_lockfile(self) -> tuple[int, int, int]:
+        pid = -1
+        thread_id = -1
+        ref_count = 0
+        try:
+            with self._lockfile.open(encoding=DEFAULT_CHARSET) as f:
+                pid = int(f.readline().strip())
+                thread_id = int(f.readline().strip())
+                ref_count = int(f.readline().strip())
+        except ValueError:
+            logger.warning(
+                "The lock file '%s' is broken.",
                 self._lockfile,
             )
             call_ktrigger(
                 IKernelTrigger.on_warning,
                 message=format_str(
                     _(
-                        "The environment '${{path}}' is already locked by PID "
-                        "${{pid}}. If you want to force unlock it, please "
-                        "remove the lock file '${{lockfile}}' manually.",
+                        "The lock file '[underline]${{lockfile}}"
+                        "[/underline]' is broken.",
                     ),
-                    fmt={
-                        "path": str(self.path),
-                        "pid": str(_("Unknown") if pid == -1 else pid),
-                        "lockfile": str(self._lockfile),
-                    },
+                    fmt={"lockfile": str(self._lockfile)},
                 ),
             )
-            if pid != -1 and not is_valid_pid(pid):
-                call_ktrigger(
-                    IKernelTrigger.on_warning,
-                    message=format_str(
-                        _(
-                            "The PID '${{pid}}' is not valid. Maybe the "
-                            "process has already exited. Remove the lock file "
-                            "if you believe the process has already exited.",
-                        ),
-                        fmt={
-                            "pid": str(pid),
-                        },
-                    ),
-                )
-
-            logger.info(
-                "Waiting for the environment to be unlocked ('%s')",
+        except OSError as exc:
+            logger.exception(
+                "Failed to read the lock file '%s'.",
                 self._lockfile,
             )
-
-            task_name = format_str(
-                _(
-                    "Waiting for the environment to be unlocked "
-                    "('${{lock_file}}'): ${{seconds}} seconds.",
+            call_ktrigger(
+                IKernelTrigger.on_warning,
+                message=format_str(
+                    _(
+                        "Failed to read the lock file '[underline]"
+                        "${{path}}[/underline]': ${{msg}}",
+                    ),
+                    fmt={"path": str(self._lockfile), "msg": str(exc)},
                 ),
-                fmt={
-                    "lock_file": str(self._lockfile),
-                },
-            )
-            call_ktrigger(
-                IKernelTrigger.on_new_task,
-                task_name=task_name,
-                task_type=IKernelTrigger.TASK_WAIT,
-                total=0,
-            )
-            wait_time = 0
-            while self.is_locked():
-                call_ktrigger(
-                    IKernelTrigger.on_progress,
-                    task_name=task_name,
-                    current=wait_time,
-                    delta=False,
-                    more_data={"env": self},
-                )
-                time.sleep(1)
-                wait_time += 1
-
-            call_ktrigger(
-                IKernelTrigger.on_finish_task,
-                task_name=task_name,
             )
 
-        pid = os.getpid()
-        logger.info(
-            "Locking the environment '%s' with PID %d.",
-            self.path,
-            pid,
-        )
-        with Path.open(
-            self.path / VENV_LOCK_FILENAME,
+        return pid, thread_id, ref_count
+
+    def _update_lock(self, pid: int, thread_id: int, ref_count: int) -> None:
+        with self._lockfile.open(
             "w",
             encoding=DEFAULT_CHARSET,
         ) as f:
-            f.write(str(pid))
+            f.write(f"{pid}\n{thread_id}\n{ref_count}\n")
+
+    def lock(self) -> None:
+        """Lock the environment."""
+        self.create()  # Ensure the environment is created.
+
+        ref_count = 0
+        if self.is_locked():
+            lock_pid, lock_thread_id, ref_count = self._parse_lockfile()
+            if self._check_alerady_locked(lock_pid, lock_thread_id):
+                ref_count = 0  # Reset the refcount if another unlocked.
+
+        ref_count += 1
+        this_pid = os.getpid()
+        this_thread_id = threading.get_ident()
+        logger.info(
+            "Locking the environment '%s' with PID %d, thread %d, "
+            "with %d refence count.",
+            self.path,
+            this_pid,
+            this_thread_id,
+            ref_count,
+        )
+        self._update_lock(this_pid, this_thread_id, ref_count)
 
     def unlock(self) -> None:
         """Unlock the environment."""
         if self.is_locked():
             logger.info("Unlocking the environment '%s'.", self.path)
-            # Missing is OK but not OK.
-            Path.unlink(self.path / VENV_LOCK_FILENAME, missing_ok=True)
+            try:
+                pid, thread_id, ref_count = self._parse_lockfile()
+                if self.is_locked_by_self() and ref_count > 1:
+                    ref_count -= 1
+                    self._update_lock(pid, thread_id, ref_count)
+                elif self.is_locked_by_self():
+                    self._lockfile.unlink()
+                else:
+                    logger.warning(
+                        "The environment '%s' is locked by another thread.",
+                        self.path,
+                    )
+                    call_ktrigger(
+                        IKernelTrigger.on_warning,
+                        message=format_str(
+                            _(
+                                "The environment '[underline]${{path}}"
+                                "[/underline]' is locked by another thread "
+                                "(PID '${{pid}}', Thread '${{thread}}')."
+                                "Unlock request is ignored.",
+                            ),
+                            fmt={
+                                "path": str(self.path),
+                                "pid": str(pid),
+                                "thread": str(thread_id),
+                            },
+                        ),
+                    )
+            except OSError as exc:
+                logger.exception(
+                    "Failed to unlock the environment '%s'.",
+                    self.path,
+                )
+                call_ktrigger(
+                    IKernelTrigger.on_warning,
+                    message=format_str(
+                        _(
+                            "Failed to unlock the extension environment "
+                            "'${{path}}': ${{msg}}",
+                        ),
+                        fmt={"path": str(self.path), "msg": str(exc)},
+                    ),
+                )
         else:
             logger.warning("The environment '%s' is not locked.", self.path)
 
@@ -234,64 +434,13 @@ class RUEnvironment:
         """Add the environment to the system path."""
         add_venv_to_syspath(self.path)
 
+    def db_is_valid(self) -> bool:
+        """Check if the database is valid."""
+        return _check_vaild_db(self.db_file)
 
-def setup_new_venv(env: RUEnvironment) -> None:
-    """Set up a new Python virtual environment for Rubisco extensions.
-
-    Args:
-        env (RUEnvironment): Path to setup the virtual environment.
-
-    """
-    logger.info("Setting up a new venv: '%s'", env.path)
-    call_ktrigger(IKernelTrigger.on_create_venv, path=env.path)
-    try:
-        if env.path.exists() and not is_venv(env.path):
-            raise RUError(
-                hint=format_str(
-                    _(
-                        "The '${{path}}' is not a valid venv but exists."
-                        "Please remove it and try again.",
-                    ),
-                    fmt={"path": str(env.path)},
-                ),
-            )
-        venv.create(env.path, with_pip=True, upgrade_deps=True)
-        (env.path / EXTENSIONS_DIR).mkdir(exist_ok=True, parents=True)
-        logger.debug("Creating database %s", env.db_file)
-        db_connection = sqlite3.connect(env.db_file)
-        db_cursor = db_connection.cursor()
-        db_cursor.execute(
-            """
-CREATE TABLE extensions (
-    name VARCHAR() PRIMARY KEY,
-    version VARCHAR(64) NOT NULL,
-    description TEXT,
-    homepage TEXT,
-    maintainers TEXT,
-    license TEXT,
-    tags TEXT,
-    requirements TEXT
-)
-            """,
-        )
-        db_connection.commit()
-        db_connection.close()
-        add_venv_to_syspath(env.path)
-    except OSError as exc:
-        raise RUOSError(
-            exc,
-            hint=_(
-                "Failed to create venv. Consider to check the"
-                "permissions of the path.",
-            ),
-        ) from exc
-    except sqlite3.Error as exc:
-        raise RUError(
-            exc,
-            hint=_(
-                "Failed to create the database for the environment.",
-            ),
-        ) from exc
+    def valid(self) -> bool:
+        """Check if the environment is valid."""
+        return is_venv(self.path) and self.db_is_valid()
 
 
 GLOBAL_ENV = RUEnvironment(GLOBAL_EXTENSIONS_VENV_DIR, EnvType.GLOBAL)
@@ -323,7 +472,7 @@ if __name__ == "__main__":
 
     if "--setup-global-env" in sys.argv:
         try:
-            setup_new_venv(GLOBAL_ENV)
+            GLOBAL_ENV.create()
         except (
             Exception  # pylint: disable=broad-exception-caught  # noqa: BLE001
         ) as exc_:
@@ -331,7 +480,7 @@ if __name__ == "__main__":
             rubisco.cli.output.show_exception(exc_)
     elif "--setup-user-env" in sys.argv:
         try:
-            setup_new_venv(USER_ENV)
+            USER_ENV.create()
         except (
             Exception  # pylint: disable=broad-exception-caught  # noqa: BLE001
         ) as exc_:
@@ -339,7 +488,7 @@ if __name__ == "__main__":
             rubisco.cli.output.show_exception(exc_)
     elif "--setup-workspace-env" in sys.argv:
         try:
-            setup_new_venv(WORKSPACE_ENV)
+            WORKSPACE_ENV.create()
         except (
             Exception  # pylint: disable=broad-exception-caught  # noqa: BLE001
         ) as exc_:
